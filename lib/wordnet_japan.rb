d2db4b1737ab6f11d4dd550a4387f9dfaa0474bd
yamasy1549 <sanae@yamasy.info>
2019-11-16 10:33:10 +0900

    bundle gem wordnet_japan

---
+++ lib/wordnet_japan.rb
@@ -0,0 +1,6 @@
+require "wordnet_japan/version"
+
+module WordnetJapan
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wordnet_japan/version.rb
@@ -0,0 +1,3 @@
+module WordnetJapan
+  VERSION = "0.1.0"
+end
